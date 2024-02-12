8e1a729c159da9cc37c7327f564453503cf23e7a
lyndasini-24 <159757890+lyndasini-24@users.noreply.github.com>
2024-02-12 09:33:02 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
                                                                                                                                        
 @app.route('/')
 def hello_world():
-    return render_template('hello.html')#comment
+    return render_template('hello.html')#commen
   
 if __name__ == "__main__":
   app.run(debug=True)
