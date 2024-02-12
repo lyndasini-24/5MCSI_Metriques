993a9a52505e2be60ee7131957ce25fc27b1207b
lyndasini-24 <159757890+lyndasini-24@users.noreply.github.com>
2024-02-12 09:57:22 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('hello.html')#commen
-  @app.route("https://lsini.alwaysdata.net/contact/")#cotact
+  @app.route("/contact/")#cotact
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>
    app.run(debug=True)
