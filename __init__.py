6c9172cd864e62aa4b7d4e9c0659a95538f0967c
lyndasini-24 <159757890+lyndasini-24@users.noreply.github.com>
2024-02-12 12:10:19 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -35,10 +35,6 @@ def mongraphique():
 def histogramme():
     return render_template("histogramme.html")
 
-@app.route("/contact/")
-def contact():
-    return render_template("contact.html")
-
 if __name__ == "__main__":
     app.run(debug=True)
   
